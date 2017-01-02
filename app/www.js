bcf8cdef48216ec764963bf76476cea0079d1179
slashhuang <627284482@qq.com>
2017-01-02 23:57:21 +0800

    anti zhihu https

--- app/www.js
+++ app/www.js
@@ -6,20 +6,21 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const shell =require('shelljs');
 
 let walkIndexPage = (url)=>{
     request.get(url,(err,res)=>{
         if(err){
             console.log(err)
         }
+        res.setEncoding('utf-8'); //防止中文乱码
         let $ = cheerio.load(res.body);
         //所有的关注者 #Profile-following => .List-item => .UserLink-link
         let queryList = $('#Profile-following');
         queryList.find('.List-item').each(function(index,item){
             let nextUrl =  $(this).find('.UserLink-link').attr('href');
             let target = `https://www.zhihu.com${nextUrl}`;
-            console.log(target);
-            walkFollower(`https://www.zhihu.com${nextUrl}`)
+            walkFollower(target)
         });
     });
 };
@@ -29,11 +30,13 @@ let saveImg = (url,name)=>{
         .on('error', function(err) {
             console.log(err)
         })
-        .pipe(fs.createWriteStream(`./resouce/img/${name}`))
+        .pipe(fs.createWriteStream(`./resource/img/${name}`))
 };
 /**
  * .ProfileHeader-main
  * .Avatar图片地址
+ *
+ *
  * .ProfileHeader-detailItem => ProfileHeader-detailLabel
  *                           => ProfileHeader-detailValue
  *                                  =>.ProfileHeader-field(去除img拿txt)
@@ -44,32 +47,23 @@ let walkFollower = (url)=>{
         if(err){
             console.log(err)
         }
+        res.setEncoding('utf-8'); //防止中文乱码
         let $ = cheerio.load(res.body);
         //信息collection
-        let detailCollection = $('.ProfileHeader-main').eq(0);
-        console.log(detailCollection.html())
-
+        let detailCollection = $('.ProfileHeader-main');
         let name = detailCollection.find('.ProfileHeader-name').text();
         let jsonData = {};
-        detailCollection.find('.ProfileHeader-detailItem')
+        detailCollection.find('.ProfileHeader-info .ProfileHeader-infoItem')
             .each(function(index,item){
-                console.log('============')
-                let labelName = item.find('.ProfileHeader-detailLabel');
-                let labelVal = '';
-                let labelValContainer = item.find('.ProfileHeader-detailValue');
-                let children = labelValContainer.children();
-                if(!children){
-                    labelVal = labelValContainer.text();
-                }else{
-                    if(labelValContainer.find('span')) {
-                        labelVal = labelValContainer.find('span').text();
-                    }else if(labelValContainer.find('.ProfileHeader-field')){
-                        labelVal = children.last().text();
-                    }
-                }
-
-                jsonData[labelName] =labelVal;
-                fs.createWriteStream(`./resouce/${name}`).write(JSON.stringify(jsonData,2,2))
+                let labelName = $(this).find('svg').attr('class').replace('Icon Icon--','');
+                let context = cheerio.load($(this).html());
+                context('div').remove();
+                console.log(context.html(),'---');
+                let labelVal = context.html();
+                jsonData[labelName] = labelVal;
+                let fileName = `./resource/${name}`;
+                // shell.touch(fileName);
+                fs.createWriteStream(fileName).write(JSON.stringify(jsonData,2,2))
         })
     });
 };
