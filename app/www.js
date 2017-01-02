564d1e87353d7f4e5fc7808b9d53a136a0833bd5
slashhuang <627284482@qq.com>
2017-01-02 23:19:30 +0800

    fix zhihu bug

--- app/www.js
+++ app/www.js
@@ -4,19 +4,22 @@
 
 
 const request = require('request');
-let cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 let walkIndexPage = (url)=>{
     request.get(url,(err,res)=>{
         if(err){
             console.log(err)
         }
-        let $ = cheerio(res);
+        let $ = cheerio.load(res.body);
         //所有的关注者 #Profile-following => .List-item => .UserLink-link
         let queryList = $('#Profile-following');
-        queryList.find('.List-item').each((item)=>{
-            let nextUrl = item.find('.UserLink-link').attr('href');
-            walkFollower(nextUrl)
+        queryList.find('.List-item').each(function(index,item){
+            let nextUrl =  $(this).find('.UserLink-link').attr('href');
+            let target = `https://www.zhihu.com${nextUrl}`;
+            console.log(target);
+            walkFollower(`https://www.zhihu.com${nextUrl}`)
         });
     });
 };
@@ -41,12 +44,33 @@ let walkFollower = (url)=>{
         if(err){
             console.log(err)
         }
-        let $ = cheerio(res);
+        let $ = cheerio.load(res.body);
         //信息collection
-        let detailCollection = $('.ProfileHeader-main .ProfileHeader-detailItem');
-        detailCollection.each((item)=>{
+        let detailCollection = $('.ProfileHeader-main').eq(0);
+        console.log(detailCollection.html())
 
+        let name = detailCollection.find('.ProfileHeader-name').text();
+        let jsonData = {};
+        detailCollection.find('.ProfileHeader-detailItem')
+            .each(function(index,item){
+                console.log('============')
+                let labelName = item.find('.ProfileHeader-detailLabel');
+                let labelVal = '';
+                let labelValContainer = item.find('.ProfileHeader-detailValue');
+                let children = labelValContainer.children();
+                if(!children){
+                    labelVal = labelValContainer.text();
+                }else{
+                    if(labelValContainer.find('span')) {
+                        labelVal = labelValContainer.find('span').text();
+                    }else if(labelValContainer.find('.ProfileHeader-field')){
+                        labelVal = children.last().text();
+                    }
+                }
+
+                jsonData[labelName] =labelVal;
+                fs.createWriteStream(`./resouce/${name}`).write(JSON.stringify(jsonData,2,2))
         })
     });
 };
-walkFollower('https://www.zhihu.com/people/huang-da-xian-14-14/followers');
+walkIndexPage('https://www.zhihu.com/people/huang-da-xian-14-14/followers');
